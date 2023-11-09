a988b232ecade5e4a007ef957d456cefce4e1198
burillu <burillu90@hotmail.it>
2023-11-09 18:53:49 +0100

    add AppHeader component

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <AppHeader />
+</template>
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  data() {
+    return {
+      msg: 'Hello World'
+    }
+  },
+  components: {
+    AppHeader
+  }
+}
+</script>
+<style>
+h1 {
+  color: red;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        header con navbar
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+
+        }
+    }, components: {
+
+    }
+}
+</script>
+
+<style scoped>
+header {
+    background-color: aquamarine;
+}
+</style>
