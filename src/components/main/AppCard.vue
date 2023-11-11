63839161eefbb8e502e176eac5c65e82937e4e13
burillu <burillu90@hotmail.it>
2023-11-11 14:44:21 +0100

    add AppCard made with props, font link in scss

---
+++ src/components/main/AppCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="card" style="width: 18rem;">
+        <img :src="thumb" class="card-img-top" :alt="series">
+        <div class="card-body">
+            <h5 class="card-title">{{ series }}</h5>
+            <p class="card-text">{{ price }}</p>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppCard',
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        thumb: String,
+        price: String,
+        series: String,
+        type: String
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
