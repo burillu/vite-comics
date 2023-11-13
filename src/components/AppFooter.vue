793d399c60cf3f00b11331aed50d477eb80ac5c2
burillu <burillu90@hotmail.it>
2023-11-13 01:50:20 +0100

    add applistfoot

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,17 +1,71 @@
 <template>
     <footer>
         <div class="container">
-            <div class="row">
-                <div class="col"></div>
-                <div class="col ">
+            <div class="row justify-space-between">
+                <div class="col-4 d-flex flex-column flex-wrap">
+
+                    <AppListFooter />
+                    <AppListFooter />
+                    <AppListFooter />
+                    <AppListFooter />
+                    <AppListFooter />
+
+
+
+                </div>
+                <div class="col-6">
                     <img src="images/dc-logo-bg.png" alt="">
                 </div>
             </div>
+
+        </div>
+        <div class="my-bg-secondary">
+            <div class="container d-flex flex-row justify-content-between align-content-center">
+
+                <button class="btn my-border text-white fw-bold text-uppercase">
+                    Sign-up Now!
+                </button>
+                <div class="d-flex align-items-center">
+                    <span class="my-text-primary text-uppercase fw-bold">
+                        Follow us
+                    </span>
+                    <nav class="">
+
+                        <ul class="nav text-secondary fw-bold  text-uppercase">
+                            <li class="nav-item">
+                                <a class="nav-link " href="#"><img src="/images/footer-facebook.png" alt="facebook"></a>
+
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link " href="#"><img src="/images/footer-twitter.png" alt="twitter"></a>
+
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link " href="#"><img src="/images/footer-youtube.png" alt="youtube"></a>
+
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link " href="#"><img src="/images/footer-pinterest.png" alt="pinterest"></a>
+
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link " href="#"><img src="/images/footer-periscope.png" alt="periscope"></a>
+
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+
+            </div>
+
+
         </div>
     </footer>
 </template>
 
 <script>
+import AppListFooter from './footer/AppListFooter.vue';
+
 export default {
     name: 'AppFooter',
     data() {
@@ -20,15 +74,46 @@ export default {
         }
     },
     components: {
-
+        AppListFooter
     }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 footer {
     color: white;
     background-image: url('/images/footer-bg.jpg');
 
+}
+
+.col-6 {
+
+
+    img {
+        width: 120%;
+
+    }
+}
+
+div.flex-wrap {
+    height: 500px;
+
+    font-family: 'open sans condensed';
+}
+
+.bg-dc {
+    background-image: url('/images/dc-logo-bg.png');
+    background-repeat: no-repeat;
+    background-size: 50vw;
+    background-position-y: -100px;
+    background-position-x: -100px;
+}
+
+.my-border {
+    border: 1px solid blue;
+}
+
+div.my-bg-secondary {
+    height: 70px;
 }
 </style>
---
+++ src/components/footer/AppListFooter.vue
@@ -0,0 +1,60 @@
+<template>
+    <ul>
+        <li class="text-white text-uppercase ">{{ title }} </li>
+        <li v-for="item in aList" :key="item.id">
+            <a :href="item.src"> {{ item.text }} </a>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'AppListFooter',
+    data() {
+        return {
+            title: 'Blablabla Titolo',
+            aList: [
+                {
+                    src: '#',
+                    text: 'blablabla',
+                    id: 1
+
+                },
+                {
+                    src: '#',
+                    text: 'blablab1la',
+                    id: 2
+
+                },
+                {
+                    src: '#',
+                    text: 'blablabl2a',
+                    id: 3
+
+                },
+                {
+                    src: '#',
+                    text: 'blablabla3',
+                    id: 4
+
+                },
+                {
+                    src: '#',
+                    text: 'blablabla4',
+                    id: 5
+
+                },
+            ]
+        }
+    }
+}
+
+</script>
+
+<style lang="scss" scoped>
+a {
+    color: gray;
+    text-decoration: none;
+
+}
+</style>
